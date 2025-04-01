85d7bc81a68aca70ee4d9094b603241a22663b90
siddhantCodes <siddhantk232@gmail.com>
2025-04-01 12:42:58 +0530

    Add nix files to get rust-toolchain and update authors list

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "FTNet";
+
+  inputs.rust-overlay = {
+    url = "github:oxalica/rust-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems
+          (system:
+            f (import nixpkgs {
+              inherit system;
+              overlays = [ (import rust-overlay) ];
+            }));
+    in
+    {
+      devShells = forAllSystems (pkgs:
+        let
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "ftnet-shell";
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+
+            ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Foundation ];
+            buildInputs = with pkgs; [
+              toolchain
+              rust-analyzer-unwrapped
+            ];
+
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          };
+        });
+
+      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+    };
+}
