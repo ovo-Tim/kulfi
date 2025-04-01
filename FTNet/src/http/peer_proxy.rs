0bea4dd9d4eb7efbef86ead389ff48be3fb1bd54
Amit Upadhyay <upadhyay@gmail.com>
2025-04-01 17:01:23 +0530

    refactor: get_stream instead of get_connection

--- FTNet/src/http/peer_proxy.rs
+++ FTNet/src/http/peer_proxy.rs
@@ -5,27 +5,37 @@ pub async fn peer_proxy(
     client_pools: ftnet::http::client::ConnectionPools,
     _patch: ftnet::http::RequestPatch,
 ) -> ftnet::http::Result {
-    let _conn = get_connection(peer_id, peer_connections, client_pools).await?;
+    let (_send, _recv) = get_stream(peer_id, peer_connections, client_pools).await?;
 
     todo!()
 }
 
-async fn get_connection(
+async fn get_stream(
     peer_id: &str,
     peer_connections: ftnet::identity::PeerConnections,
     client_pools: ftnet::http::client::ConnectionPools,
-) -> eyre::Result<bb8::Pool<ftnet::Identity>> {
+) -> eyre::Result<(iroh::endpoint::SendStream, iroh::endpoint::RecvStream)> {
     let mut peers = peer_connections.lock().await;
 
-    match peers.get(peer_id) {
-        Some(v) => Ok(v.clone()),
+    let pool = match peers.get(peer_id) {
+        Some(v) => v.clone(),
         None => {
             let pool = bb8::Pool::builder()
                 .build(ftnet::Identity::from_id52(peer_id, client_pools)?)
                 .await?;
 
             peers.insert(peer_id.to_string(), pool.clone());
-            Ok(pool)
+            pool
         }
-    }
+    };
+
+    Ok(pool
+        .get()
+        .await
+        .map_err(|e| {
+            eprintln!("failed to get connection: {e:?}");
+            eyre::anyhow!("failed to get connection: {e:?}")
+        })?
+        .open_bi()
+        .await?)
 }
