7b9f347da2eed8ebd07841cf22d78080eb667fa0
Amit Upadhyay <upadhyay@gmail.com>
2025-04-07 19:05:41 +0530

    started cli parsing

--- skynet/src/main.rs
+++ skynet/src/main.rs
@@ -1,6 +1,41 @@
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    println!("Hello, world!");
+    use clap::Parser;
+
+    // run with RUST_LOG="skynet=info" to only see our logs when running with the --trace flag
+    tracing_subscriber::fmt::init();
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::ExposeHttp { port, secure } => {
+            tracing::info!("Exposing HTTP service on FTNet with port: {port}, secure: {secure}.");
+        }
+    }
 
     Ok(())
+}
+
+#[derive(clap::Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[arg(long, global = true)]
+    pub trace: bool,
+
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(clap::Subcommand, Debug)]
+pub enum Command {
+    #[clap(about = "Expose HTTP Service on FTNet, connect using FTNet")]
+    ExposeHttp {
+        port: u16,
+        #[arg(
+            long,
+            default_value_t = false,
+            help = "Use this if the service is HTTPS"
+        )]
+        secure: bool,
+    },
 }
