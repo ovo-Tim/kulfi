759f15ad0f97b9d5c00c7a97cf30d841993fab9d
Amit Upadhyay <upadhyay@gmail.com>
2025-04-11 13:58:12 +0530

    hashmap should contain (self_id, remote_id)

--- ftnet-utils/src/get_stream2.rs
+++ ftnet-utils/src/get_stream2.rs
@@ -1,8 +1,9 @@
 /// PeerConnections stores the iroh connections for every peer.
 ///
 /// when a connection is broken, etc., we remove the connection from the map.
-pub type PeerStreamSenders =
-std::sync::Arc<tokio::sync::Mutex<std::collections::HashMap<RemoteID52, StreamRequestSender>>>;
+pub type PeerStreamSenders = std::sync::Arc<
+    tokio::sync::Mutex<std::collections::HashMap<(SelfID52, RemoteID52), StreamRequestSender>>,
+>;
 
 type Stream = (iroh::endpoint::SendStream, ftnet_utils::FrameReader);
 type StreamResult = eyre::Result<Stream>;
@@ -10,12 +11,9 @@ type ReplyChannel = tokio::sync::oneshot::Sender<StreamResult>;
 #[expect(unused)]
 type ReplyChannelReceiver = tokio::sync::oneshot::Receiver<StreamResult>;
 type RemoteID52 = String;
+type SelfID52 = String;
 
-type StreamRequest = (
-    iroh::Endpoint,
-    ftnet_utils::Protocol,
-    ReplyChannel,
-);
+type StreamRequest = (iroh::Endpoint, ftnet_utils::Protocol, ReplyChannel);
 
 type StreamRequestSender = tokio::sync::mpsc::Sender<StreamRequest>;
 type StreamRequestReceiver = tokio::sync::mpsc::Receiver<StreamRequest>;
@@ -33,45 +31,48 @@ type StreamRequestReceiver = tokio::sync::mpsc::Receiver<StreamRequest>;
 pub async fn get_stream(
     self_endpoint: iroh::Endpoint,
     protocol: ftnet_utils::Protocol,
-    remote_node_id52: &RemoteID52,
+    remote_node_id52: RemoteID52,
     peer_stream_senders: PeerStreamSenders,
 ) -> eyre::Result<(iroh::endpoint::SendStream, ftnet_utils::FrameReader)> {
+    let self_id52 = ftnet_utils::public_key_to_id52(&self_endpoint.node_id());
+
     let stream_request_sender =
-        get_stream_request_sender(remote_node_id52, peer_stream_senders.clone()).await?;
+        get_stream_request_sender(self_id52, remote_node_id52, peer_stream_senders).await?;
 
     let (reply_channel, receiver) = tokio::sync::oneshot::channel();
 
     stream_request_sender
-        .send((
-            self_endpoint,
-            protocol,
-            reply_channel,
-        ))
+        .send((self_endpoint, protocol, reply_channel))
         .await?;
 
     receiver.await?
 }
 
 async fn get_stream_request_sender(
-    remote_node_id52: &RemoteID52,
+    self_id52: SelfID52,
+    remote_node_id52: RemoteID52,
     peer_stream_senders: PeerStreamSenders,
 ) -> eyre::Result<StreamRequestSender> {
     let mut peer_stream_senders = peer_stream_senders.lock().await;
 
-    if let Some(sender) = peer_stream_senders.get(remote_node_id52) {
+    if let Some(sender) = peer_stream_senders.get(&(self_id52.clone(), remote_node_id52.clone())) {
         return Ok(sender.clone());
     }
 
+    // TODO: figure out if the mpsc::channel is the right size
     let (sender, receiver) = tokio::sync::mpsc::channel(1);
-    peer_stream_senders.insert(remote_node_id52.clone(), sender.clone());
+    peer_stream_senders.insert((self_id52, remote_node_id52.clone()), sender.clone());
 
-    let remote_node_id52 = remote_node_id52.to_owned();
+    let remote_node_id52 = remote_node_id52;
 
     tokio::spawn(async move { connection_manager_worker(receiver, remote_node_id52).await });
 
     Ok(sender)
 }
 
-async fn connection_manager_worker(_receiver: StreamRequestReceiver, _remote_node_id52: RemoteID52) {
+async fn connection_manager_worker(
+    _receiver: StreamRequestReceiver,
+    _remote_node_id52: RemoteID52,
+) {
     todo!()
 }
