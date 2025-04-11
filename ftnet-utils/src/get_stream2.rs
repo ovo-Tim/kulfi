9a548244a972ae66376fac971b888d919273b4e5
Amit Upadhyay <upadhyay@gmail.com>
2025-04-11 13:23:44 +0530

    get_sender2::get_stream_request_sender()

--- ftnet-utils/src/get_stream2.rs
+++ ftnet-utils/src/get_stream2.rs
@@ -1,43 +1,77 @@
 /// PeerConnections stores the iroh connections for every peer.
 ///
 /// when a connection is broken, etc., we remove the connection from the map.
-#[expect(unused)]
 pub type PeerStreamSenders =
-std::sync::Arc<tokio::sync::Mutex<std::collections::HashMap<String, StreamRequestSender>>>;
+std::sync::Arc<tokio::sync::Mutex<std::collections::HashMap<RemoteID52, StreamRequestSender>>>;
 
 type Stream = (iroh::endpoint::SendStream, ftnet_utils::FrameReader);
 type StreamResult = eyre::Result<Stream>;
-#[expect(unused)]
 type ReplyChannel = tokio::sync::oneshot::Sender<StreamResult>;
+#[expect(unused)]
 type ReplyChannelReceiver = tokio::sync::oneshot::Receiver<StreamResult>;
 type RemoteID52 = String;
 
 type StreamRequest = (
     iroh::Endpoint,
-    RemoteID52,
     ftnet_utils::Protocol,
-    ReplyChannelReceiver,
+    ReplyChannel,
 );
 
 type StreamRequestSender = tokio::sync::mpsc::Sender<StreamRequest>;
-#[expect(unused)]
 type StreamRequestReceiver = tokio::sync::mpsc::Receiver<StreamRequest>;
 
-/// get_stream takes the protocol as well, as every outgoing bi-direction stream must have a
-/// protocol. get_stream tries to check if the bidirectional stream is healthy, as simply opening
+/// get_stream tries to check if the bidirectional stream is healthy, as simply opening
 /// a bidirectional stream, or even simply writing on it does not guarantee that the stream is
-/// open. only the read request times out to tell us something is wrong.
+/// open. only the read request times out to tell us something is wrong. this is why get_stream
+/// takes the protocol as well, as every outgoing bi-direction stream must have a protocol. it
+/// sends the protocol and waits for an ack. if the ack is not received within a certain time, it
+/// assumes the connection is not healthy, and tries to recreate the connection.
 ///
-/// so solve this, we send a protocol message on the stream, and wait for an acknowledgement. if we
-/// do not get the ack almost right away on a connection that we got from the cache, we assume the
-/// connection is not healthy, and we try to recreate the connection. if it is a fresh connection,
-/// then we use a longer timeout.
+/// for managing connection, we use a spawned task. this task listens for incoming stream requests
+/// and manages the connection as part of the task local data.
 #[expect(unused)]
 pub async fn get_stream(
-    _self_endpoint: iroh::Endpoint,
-    _protocol: ftnet_utils::Protocol,
-    _remote_node_id52: &str,
-    _peer_connections: ftnet_utils::PeerStreamSenders,
+    self_endpoint: iroh::Endpoint,
+    protocol: ftnet_utils::Protocol,
+    remote_node_id52: &RemoteID52,
+    peer_stream_senders: PeerStreamSenders,
 ) -> eyre::Result<(iroh::endpoint::SendStream, ftnet_utils::FrameReader)> {
+    let stream_request_sender =
+        get_stream_request_sender(remote_node_id52, peer_stream_senders.clone()).await?;
+
+    let (reply_channel, receiver) = tokio::sync::oneshot::channel();
+
+    stream_request_sender
+        .send((
+            self_endpoint,
+            protocol,
+            reply_channel,
+        ))
+        .await?;
+
+    receiver.await?
+}
+
+async fn get_stream_request_sender(
+    remote_node_id52: &RemoteID52,
+    peer_stream_senders: PeerStreamSenders,
+) -> eyre::Result<StreamRequestSender> {
+    let mut peer_stream_senders = peer_stream_senders.lock().await;
+
+    if let Some(sender) = peer_stream_senders.get(remote_node_id52) {
+        return Ok(sender.clone());
+    }
+
+    let (sender, receiver) = tokio::sync::mpsc::channel(1);
+    peer_stream_senders.insert(remote_node_id52.clone(), sender.clone());
+
+    let remote_node_id52 = remote_node_id52.to_owned();
+
+    tokio::spawn(async move { connection_manager_worker(receiver, remote_node_id52).await });
+
+    Ok(sender)
+}
+
+async fn connection_manager_worker(_receiver: StreamRequestReceiver, _remote_node_id52: RemoteID52) {
     todo!()
 }
