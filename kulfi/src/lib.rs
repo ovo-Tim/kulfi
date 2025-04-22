ee7a67122c6fb659e4f00a69ee0993ef42a30c0f
Amit Upadhyay <upadhyay@gmail.com>
2025-04-22 12:28:14 +0530

    cargo fmt

--- kulfi/src/lib.rs
+++ kulfi/src/lib.rs
@@ -8,9 +8,9 @@ extern crate self as kulfi; // TODO: Remove this and separate kulfi binary from 
 use clap as _;
 use clap_verbosity_flag as _;
 use directories as _;
-use tracing_subscriber as _;
 #[cfg(target_os = "linux")]
 use libdbus_sys as _;
+use tracing_subscriber as _;
 
 mod config;
 pub mod control_server;
