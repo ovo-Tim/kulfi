1de2b49c60fddd9921cda487d418072136365539
Amit Upadhyay <upadhyay@gmail.com>
2025-04-25 14:42:37 +0530

    proper check

--- malai.sh/install.sh
+++ malai.sh/install.sh
@@ -165,8 +165,8 @@ setup() {
 
 check_architecture() {
     if [ "$(uname)" = "Darwin" ]; then
-        ARCH=$(uname -m)
-        if [ "$ARCH" = *"x86_64"* ]; then
+        ARCH=$(arch)
+        if [ "$ARCH" = "x86_64" ]; then
             log_error "Intel-based Macs are not yet supported."
             exit 1
         fi
