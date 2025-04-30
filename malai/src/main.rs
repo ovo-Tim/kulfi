613943f0e1f68a92796076f63a14daaba2300907
siddhantCodes <siddhantk232@gmail.com>
2025-04-30 15:22:29 +0530

    Fix: Build windows cli for now
    The UI feature is not ready and we need cli till then. Having
    `windows_subsystem = "windows"` prevents it from running in a cmd
    prompt/pwsh on windows because it's a GUI app.

--- malai/src/main.rs
+++ malai/src/main.rs
@@ -1,5 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(all(not(debug_assertions), feature = "ui"), windows_subsystem = "windows")]
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
