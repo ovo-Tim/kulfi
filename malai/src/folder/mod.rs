d2d0f154b0860afb30b2d6c187cfe4da49b7053e
Amit Upadhyay <upadhyay@gmail.com>
2025-05-03 03:01:46 +0530

    refactor: promote folder to a module for better organisation

--- malai/src/folder/mod.rs
+++ malai/src/folder/mod.rs
@@ -1,3 +1,5 @@
+mod render_folder;
+
 /// folder() exposes a folder on the kulfi network
 ///
 /// the folder needs a little bit of user interface, the directory listing page. there are many
@@ -27,46 +29,4 @@
 /// will compile `folder.html` template as part of the build process.
 pub async fn folder(_path: String, _graceful: kulfi_utils::Graceful) -> eyre::Result<()> {
     todo!()
-}
-
-/// We will use few templates actually, skeleton.html, show-folder.html, show-file.html. We
-/// will loop through each folder and file using the show-file/folder, and then pass the joined
-/// HTML to folder-skeleton.html.
-fn render_folder(path: &str) -> eyre::Result<String> {
-    let mut html = String::new();
-    // for content of this folder
-    for entry in std::fs::read_dir(path)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_dir() {
-            html.push_str(&show_folder(path)?);
-        } else {
-            html.push_str(&show_file(path)?);
-        }
-    }
-
-    return Ok(format!(
-        include_str!("../templates/folder/skeleton.html"),
-        content = html,
-        path = path,
-    ));
-
-    macro_rules! render_template {
-        ($file:expr, $path:expr) => {{
-            let path = $path
-                .file_name()
-                .and_then(|s| s.to_str())
-                .ok_or_else(|| eyre::anyhow!("failed to convert path to str"))?;
-
-            Ok(format!(include_str!($file), path = path))
-        }};
-    }
-
-    fn show_file(path: std::path::PathBuf) -> eyre::Result<String> {
-        render_template!("../templates/folder/show-file.html", path)
-    }
-
-    fn show_folder(path: std::path::PathBuf) -> eyre::Result<String> {
-        render_template!("../templates/folder/show-folder.html", path)
-    }
 }
