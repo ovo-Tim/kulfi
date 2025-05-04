201c0a38bab6d8f23ccd025cbe166759b13579e8
Amit Upadhyay <upadhyay@gmail.com>
2025-05-05 00:39:53 +0530

    proper way to do async code in tauri protocol handler

--- kulfi/src/tauri.rs
+++ kulfi/src/tauri.rs
@@ -2,18 +2,14 @@
 pub fn ui() -> eyre::Result<()> {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .register_asynchronous_uri_scheme_protocol("kulfi", |_ctx, request, responder| {
-            // let request = kulfi_utils::http::vec_u8_to_bytes(request);
-            // responder.respond(kulfi_utils::http::response_to_static(Ok(
-            //     kulfi_utils::http_to_peer(request).await.unwrap(),
-            // )))
-
-            let path = request.uri().to_string();
-            responder.respond(
-                http::Response::builder()
-                    .body(format!("yo: {path}").into_bytes())
-                    .unwrap(),
-            );
+        .register_asynchronous_uri_scheme_protocol("kulfi", |_ctx, request, _responder| {
+            tauri::async_runtime::spawn(async move {
+                let _request = kulfi_utils::http::vec_u8_to_bytes(request);
+                // responder.respond(kulfi_utils::http::response_to_static(Ok(
+                //     kulfi_utils::http_to_peer(request).await.unwrap(),
+                // )))
+                todo!()
+            });
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
